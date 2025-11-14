33db1dac7e5a4c74da2c8d8e81e7361daaefc773
GuedesSiilva <davig0400@gmail.com>
2025-11-14 08:42:33 -0300

    atv 12

--- src/routes/avaliacoes.routes.js
+++ src/routes/avaliacoes.routes.js
@@ -3,12 +3,12 @@ import { listarAvaliacoes, criarAvaliacao ,  ListarAvaliacoesDeLivros} from "../
 
 const router = express.Router();
 
-// Rota para listar todas as avaliações
+
 router.get("/", listarAvaliacoes);
 
 router.get("/media", ListarAvaliacoesDeLivros);
 
-// Rota para criar uma nova avaliação
+
 router.post("/", criarAvaliacao);
 
 
